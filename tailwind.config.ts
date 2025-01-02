751a0ee3def6e9abbc18bcb3f58f8a04b6e5a8ae
Zack Dolin <zack.dolin@akqa.com>
2025-01-02 15:42:20 -0500

    add surface classes and values

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        blue: {
+          950: '#000f18',
+        },
+        sky: {
+          100: '#dfeef7',
+          50: '#f0f7fc'
+        },
+        yellow: {
+          50: '#fffcf0',
+          950: '#0f0c00',
+        },
+        neutral: {
+          500: '#6C6C6C',
+        },
+        gray: {
+          800: '#1f1f1f',
+        },
+        zinc: {
+          900: '#1f1f1f',
+        },
+      },
       fontFamily: {
         sans: ['var(--font-general-sans), sans-serif'],
       },
