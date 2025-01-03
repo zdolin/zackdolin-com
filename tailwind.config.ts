200372816751d5fcc17ace9fd836c948cefd0868
Zack Dolin <zack.dolin@akqa.com>
2025-01-02 21:29:27 -0500

    add tailwind-animate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,49 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        blue: {
-          950: '#000f18',
-        },
-        sky: {
-          100: '#dfeef7',
-          50: '#f0f7fc'
-        },
-        yellow: {
-          50: '#fffcf0',
-          950: '#0f0c00',
-        },
-        neutral: {
-          500: '#6C6C6C',
-        },
-        gray: {
-          800: '#1f1f1f',
-        },
-        zinc: {
-          900: '#1f1f1f',
-        },
-      },
-      fontFamily: {
-        sans: ['var(--font-general-sans), sans-serif'],
-      },
-    },
+  	extend: {
+  		colors: {
+  			blue: {
+  				'950': '#000f18'
+  			},
+  			sky: {
+  				'50': '#f0f7fc',
+  				'100': '#dfeef7',
+					'600': '#007bc7',
+  			},
+  			yellow: {
+  				'50': '#fffcf0',
+  				'950': '#0f0c00'
+  			},
+  			neutral: {
+  				'500': '#6C6C6C'
+  			},
+  			gray: {
+  				'800': '#1f1f1f'
+  			},
+  			zinc: {
+  				'900': '#1f1f1f'
+  			}
+  		},
+  		fontFamily: {
+  			sans: [
+  				'var(--font-general-sans), sans-serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
