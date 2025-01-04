d0415da642253ec6ff30080ddb8fcaf7b2f4379f
Zack Dolin <zack.dolin@akqa.com>
2025-01-03 21:20:39 -0500

    feat: add Label

---
+++ src/components/atoms/Label/Label.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Label } from './Label';
+
+const meta: Meta = {
+  title: 'atoms/Label',
+  tags: ['autodocs'],
+  argTypes: {
+    level: {
+      control: {
+        type: 'select',
+        options: [1, 2, 3, 4, 5, 6],
+      },
+      defaultValue: 1,
+    },
+    className: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: '',
+    },
+    children: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Default Text',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<{ level?: 1 | 2 | 3 | 4 | 5 | 6; className?: string; children: string }>;
+
+export const DefaultLabel: Story = {
+  render: (args) => <Label {...args} />,
+  args: {
+    level: 1,
+    children: 'Default Label',
+  },
+};
+
+export const LabelCustomClassName: Story = {
+  render: (args) => <Label {...args} />,
+  args: {
+    level: 3,
+    className: '!text-red-500',
+    children: 'Custom Styled Label',
+  },
+};
---
+++ src/components/atoms/Label/Label.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx';
+
+type LabelProps = {
+  level?: 1 | 2 | 3 | 4 | 5 | 6;
+} & React.ComponentPropsWithoutRef<'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>;
+
+export function Label({ className, level = 1, ...props }: LabelProps) {
+  let Element: `h${typeof level}` = `h${level}`;
+
+  return (
+    <Element
+      {...props}
+      className={clsx(
+        'text-sm font-semibold uppercase text-sky-600 md:text-xs lg:text-base',
+        className
+      )}
+    />
+  );
+}
