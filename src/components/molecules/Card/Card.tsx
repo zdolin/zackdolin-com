297af8eff6f9a0e1fae037bdcdce6c88359f8aaf
Zack Dolin <zack.dolin@akqa.com>
2025-01-09 21:50:39 -0500

    fix: apply font-medium

--- src/components/molecules/Card/Card.tsx
+++ src/components/molecules/Card/Card.tsx
@@ -15,14 +15,14 @@ const Card = ({ index, title, description, date, location }: CardProps) => (
     )}
   >
     <div>
-      <h3 className="text-primary text-center text-base md:text-left md:text-lg md:font-semibold lg:text-2xl">
+      <h3 className="text-primary text-center text-base font-medium md:text-left md:text-lg md:font-semibold lg:text-2xl">
         {title}
       </h3>
       <p className="text-secondary text-center text-base md:text-left md:text-sm lg:text-lg">
         {description}
       </p>
     </div>
-    <aside>
+    <aside className="mb-4 md:mb-0">
       <h4 className="text-primary text-center text-sm uppercase md:text-right lg:text-base">
         {date}
       </h4>
