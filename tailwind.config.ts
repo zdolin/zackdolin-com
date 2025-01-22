25f202411c9742916547c937f4804c5c693a9a5e
Zack Dolin <zack.dolin@akqa.com>
2025-01-21 21:22:24 -0500

    feat: add gray-200

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,7 @@ const config: Config = {
           '500': '#6C6C6C',
         },
         gray: {
+          '200': '#ededed',
           '300': '#dcdcdc',
           '500': '#aeaeae',
           '700': '#1f1f1f',
