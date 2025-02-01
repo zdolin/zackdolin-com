ff61e692d96e3ac8ac766db6e8a655bdccc9a635
Zack Dolin <zack.dolin@akqa.com>
2025-02-01 14:56:12 -0500

    refactor: remove extra element

--- src/components/organisms/ResumeSection/ResumeSection.tsx
+++ src/components/organisms/ResumeSection/ResumeSection.tsx
@@ -8,27 +8,25 @@ export interface ResumeSectionProps {
 }
 
 const ResumeSection = ({ data }: ResumeSectionProps) => (
-  <>
-    <SectionWrapper
-      className="mb-6"
-      iconType={data.categoryIcon as keyof typeof iconMap}
-      category={data.category}
-      heading={data.heading}
-      body={data.body}
-    >
-      {data.experienceList.map((item: any, index: number) => (
-        <Card className="mb-4" key={index} {...item} index={index} />
-      ))}
-      <HeadingWithBody
-        className="pb-4 pt-8 md:pt-10"
-        heading={data.heading2}
-        body={data.body2}
-      />
-      {data.educationList.map((item: any, index: number) => (
-        <Card className="mb-4" key={index} {...item} index={index} />
-      ))}
-    </SectionWrapper>
-  </>
+  <SectionWrapper
+    className="mb-6"
+    iconType={data.categoryIcon as keyof typeof iconMap}
+    category={data.category}
+    heading={data.heading}
+    body={data.body}
+  >
+    {data.experienceList.map((item: any, index: number) => (
+      <Card className="mb-4" key={index} {...item} index={index} />
+    ))}
+    <HeadingWithBody
+      className="pb-4 pt-8 md:pt-10"
+      heading={data.heading2}
+      body={data.body2}
+    />
+    {data.educationList.map((item: any, index: number) => (
+      <Card className="mb-4" key={index} {...item} index={index} />
+    ))}
+  </SectionWrapper>
 );
 
 export default ResumeSection;
