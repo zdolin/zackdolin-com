a6e5e6bc855f7acab3fadacd2b11259fbdf7f5b5
Zack Dolin <zack.dolin@akqa.com>
2025-02-03 20:17:50 -0500

    fix: fix mobile nav

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -3,10 +3,10 @@
 import ZLogo from '@/assets/images/z-logo.svg';
 import Button from '@/components/atoms/Button/Button';
 import DarkModeToggle from '@/components/atoms/DarkModeToggle';
-import { Dialog, DialogPanel } from '@headlessui/react';
+import { Dialog, DialogPanel, Transition } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 interface NavigationItem {
   name: string;
@@ -55,60 +55,65 @@ export default function Header({ className = '' }: HeaderProps) {
           <Button>Contact me</Button>
           <DarkModeToggle />
         </div>
-        <div className="flex md:hidden">
+        <div className="relative z-50 flex md:hidden">
           <button
             type="button"
-            onClick={() => setMobileMenuOpen(true)}
+            onClick={() => setMobileMenuOpen((prev) => !prev)}
             className="text-primary -m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
           >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
+            <span className="sr-only">
+              {mobileMenuOpen ? 'Close main menu' : 'Open main menu'}
+            </span>
+            {mobileMenuOpen ? (
+              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+            ) : (
+              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            )}
           </button>
         </div>
       </nav>
-      <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="md:hidden"
-      >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center gap-x-6">
-            <Button>Contact me</Button>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="text-primary -m-2.5 rounded-md p-2.5"
+
+      <Transition.Root show={mobileMenuOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-40 md:hidden"
+          onClose={setMobileMenuOpen}
+        >
+          <div
+            className="fixed inset-0 bg-black bg-opacity-25"
+            aria-hidden="true"
+          />
+          <div className="fixed inset-0 flex justify-end">
+            <Transition.Child
+              as={Fragment}
+              enter="transition ease-out duration-300 transform"
+              enterFrom="translate-x-full"
+              enterTo="translate-x-0"
+              leave="transition ease-in duration-300 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="translate-x-full"
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
-            </button>
+              <DialogPanel className="surface-primary fixed bottom-0 right-0 top-16 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <div className="mt-8 flow-root">
+                  <div className="-my-6 divide-y divide-gray-500/10 dark:divide-gray-400">
+                    <div className="space-y-2 py-6">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className="-mx-3 block rounded-lg px-3 py-3 text-base font-normal text-black hover:bg-gray-50 dark:text-white dark:hover:bg-gray-800"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </DialogPanel>
+            </Transition.Child>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-normal text-black hover:bg-gray-50 dark:text-white"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-normal text-black hover:bg-gray-50 dark:text-white"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
+        </Dialog>
+      </Transition.Root>
     </header>
   );
 }
