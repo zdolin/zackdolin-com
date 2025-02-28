766e87b9866df6a79e1bf06c79f2568b0a1d68f0
Zack Dolin <zack.dolin@akqa.com>
2025-02-27 20:48:11 -0500

    feat: add testimonials section

--- src/components/organisms/TestimonialsSection/TestimonialsSection.tsx
+++ src/components/organisms/TestimonialsSection/TestimonialsSection.tsx
@@ -4,9 +4,11 @@ import Button from '@/components/atoms/Button';
 import Icon, { iconMap } from '@/components/atoms/Icon';
 import CardTestimonial from '@/components/molecules/CardTestimonial';
 import SectionWrapper from '@/components/molecules/SectionWrapper';
+import { EASE_OUT_QUART } from '@/constants/easing';
 import { TestimonialItemType } from '@/types/component';
 import clsx from 'clsx';
 import useEmblaCarousel from 'embla-carousel-react';
+import { motion } from 'framer-motion';
 import { useCallback, useEffect, useState } from 'react';
 
 export interface TestimonialsSectionProps {
@@ -73,37 +75,66 @@ const TestimonialsSection = ({ data }: TestimonialsSectionProps) => {
         </div>
 
         {/* Navigation Arrows */}
-        <Button
-          className={clsx(
-            'absolute bottom-0 left-0 z-10 -mb-6 rounded-full !p-3',
-            'disabled:cursor-not-allowed disabled:opacity-50',
-            'scale-75 transform lg:scale-90'
-          )}
-          onClick={scrollPrev}
-          disabled={!prevBtnEnabled}
-          aria-label="Previous"
-          hideArrow
+        <motion.div
+          className="absolute bottom-0 left-0 z-10 -mb-6"
+          initial={{ opacity: 0, x: '100%' }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 0.9,
+            ease: EASE_OUT_QUART,
+          }}
+          viewport={{ once: true, margin: '0px 0px -5% 0px' }}
         >
-          <Icon type="chevronLeft" />
-        </Button>
-        <Button
-          className={clsx(
-            'absolute bottom-0 right-0 z-10 -mb-6 rounded-full !p-3',
-            'disabled:cursor-not-allowed disabled:opacity-50',
-            'scale-75 transform lg:scale-90'
-          )}
-          onClick={scrollNext}
-          disabled={!nextBtnEnabled}
-          aria-label="Next"
-          hideArrow
+          <Button
+            className={clsx(
+              'rounded-full !p-3',
+              'disabled:cursor-not-allowed disabled:opacity-50',
+              'scale-75 transform lg:scale-90'
+            )}
+            onClick={scrollPrev}
+            disabled={!prevBtnEnabled}
+            aria-label="Previous"
+            hideArrow
+          >
+            <Icon type="chevronLeft" />
+          </Button>
+        </motion.div>
+        <motion.div
+          className="absolute bottom-0 right-0 z-10 -mb-6"
+          initial={{ opacity: 0, x: '-100%' }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{
+            duration: 0.9,
+            ease: EASE_OUT_QUART,
+          }}
+          viewport={{ once: true, margin: '0px 0px -5% 0px' }}
         >
-          <Icon type="chevronRight" />
-        </Button>
+          <Button
+            className={clsx(
+              'rounded-full !p-3',
+              'disabled:cursor-not-allowed disabled:opacity-50',
+              'scale-75 transform lg:scale-90'
+            )}
+            onClick={scrollNext}
+            disabled={!nextBtnEnabled}
+            aria-label="Next"
+            hideArrow
+          >
+            <Icon type="chevronRight" />
+          </Button>
+        </motion.div>
 
         {/* Dots Navigation */}
         <div className="mt-8 flex justify-center">
           {data.testimonialList.map((_: any, index: number) => (
-            <button
+            <motion.button
+              initial={{ opacity: 0, scaleX: 0.5 }}
+              whileInView={{ opacity: 1, scaleX: 1 }}
+              transition={{
+                duration: 0.9,
+                ease: EASE_OUT_QUART,
+              }}
+              viewport={{ once: true, margin: '0px 0px -5% 0px' }}
               key={index}
               className={clsx(
                 'mx-2 h-1 w-12 rounded-full md:h-1.5 md:w-6 lg:mx-3 lg:w-[3.75rem]',
