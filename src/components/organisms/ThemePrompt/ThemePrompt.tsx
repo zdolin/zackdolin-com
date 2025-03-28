c05fef4872eb24085cc45fae8635d1ca3c22dde5
Zack Dolin <zack.dolin@akqa.com>
2025-03-27 21:06:52 -0400

    feat: create form and submit handler

--- src/components/organisms/ThemePrompt/ThemePrompt.tsx
+++ src/components/organisms/ThemePrompt/ThemePrompt.tsx
@@ -10,6 +10,7 @@ const THEME_PROMPT_KEY = 'themePromptDismissed';
 
 const ThemePrompt = () => {
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     //const dismissed = localStorage.getItem(THEME_PROMPT_KEY);
@@ -24,6 +25,11 @@ const ThemePrompt = () => {
     localStorage.setItem(THEME_PROMPT_KEY, 'true');
   };
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+  };
+
   return (
     <ModalOrDrawer
       open={open}
@@ -31,38 +37,37 @@ const ThemePrompt = () => {
       title="Customize the Vibe"
       className="w-full md:max-w-2xl lg:max-w-4xl"
     >
-      <div className="space-y-8 p-4 text-sm text-text-primary lg:space-y-10">
+      <form
+        className="space-y-7 p-4 text-sm text-text-primary lg:space-y-9"
+        onSubmit={handleSubmit}
+      >
         <div className="space-y-2 lg:space-y-4">
           <Heading>
-            This site adapts to your style! 🎨
+            Retheme this site with a prompt! 🎨
             <br />
           </Heading>
           <p className="text-sm text-text-accent md:text-base lg:text-xl">
-            Describe a theme you&apos;d like to see, and watch the colors shift
-            to match your mood.
+            Describe a look or feel, and the site will adapt its color palette
+            to match.
           </p>
         </div>
         <Textarea
           rows={3}
           placeholder={
-            'Try "Retro synthwave with forest green" or "A light and airy seaside vibe"'
+            'Try "Sunset desert tones" or "Retro synthwave with forest green"'
           }
           resizable={false}
+          required
         />
         <div className="flex justify-end space-x-3">
           <Button intent="secondary" onClick={handleClose} hideArrow>
             Not now
           </Button>
-          <Button
-            onClick={() => {
-              // TODO: Trigger theme generation here
-              handleClose();
-            }}
-          >
-            Apply Theme
+          <Button type="submit" disabled={loading}>
+            {loading ? 'Applying Theme...' : 'Apply Theme'}
           </Button>
         </div>
-      </div>
+      </form>
     </ModalOrDrawer>
   );
 };
