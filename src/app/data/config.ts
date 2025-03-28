ea24e2b6f6f6aed3eb08d8efa8c7e1974fcaf14e
Zack Dolin <zack.dolin@akqa.com>
2025-03-27 22:49:57 -0400

    fix: show correct header nav order

--- src/app/data/config.ts
+++ src/app/data/config.ts
@@ -3,7 +3,7 @@ import { NavigationItemType } from "@/types/component";
 export const navigationList: NavigationItemType[] = [
   { text: 'Home', href: '#introduction', icon: 'home' },
   { text: 'Skills', href: '#skills', icon: 'skills' },
-  { text: 'Resume', href: '#resume', icon: 'resume' },
   { text: 'Projects', href: '#projects', icon: 'portfolio' },
+  { text: 'Resume', href: '#resume', icon: 'resume' },
   { text: 'Testimonials', href: '#testimonials', icon: 'testimonials' },
 ];
