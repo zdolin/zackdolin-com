5ef2e068fe52bbf713bddaafc5e952f31a885b66
Zack Dolin <zack.dolin@akqa.com>
2025-03-27 20:43:45 -0400

    refactor: increase visibility in dark mode

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -31,7 +31,7 @@ export const Input = forwardRef(function Input(
         // Basic layout
         'relative block w-full',
         // Focus ring
-        'dark:after:focus-within:ring-button-active after:pointer-events-none after:absolute after:inset-0 after:rounded-2xl after:ring-inset after:ring-transparent sm:after:focus-within:ring-2 sm:after:focus-within:ring-button-hover',
+        'after:pointer-events-none after:absolute after:inset-0 after:rounded-2xl after:ring-inset after:ring-transparent dark:after:focus-within:ring-button-active sm:after:focus-within:ring-2 sm:after:focus-within:ring-button-hover',
         // Disabled state
         'has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-zinc-950/5 before:has-[[data-disabled]]:shadow-none',
         // Invalid state
@@ -61,7 +61,7 @@ export const Input = forwardRef(function Input(
           // Basic layout
           'relative block w-full appearance-none rounded-2xl px-[calc(theme(spacing[4])-1px)] py-[calc(theme(spacing[3])-1px)] sm:px-[calc(theme(spacing[4])-1px)] sm:py-[calc(theme(spacing[2])-1px)]',
           // Typography
-          'text-text-input text-lg placeholder:text-zinc-500',
+          'text-lg text-text-input placeholder:text-zinc-500 dark:placeholder:text-zinc-400',
           // Border
           'border border-zinc-950/10 data-[hover]:border-zinc-950/20 dark:border-white/10 dark:data-[hover]:border-white/20',
           // Background color
