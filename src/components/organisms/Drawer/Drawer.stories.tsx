8d2f3561f7b43c8ec0c7653e27a7f69ad5d73b80
Zack Dolin <zack.dolin@akqa.com>
2025-04-01 21:55:48 -0400

    fix: fix stories

--- src/components/organisms/Drawer/Drawer.stories.tsx
+++ src/components/organisms/Drawer/Drawer.stories.tsx
@@ -16,6 +16,57 @@ export default meta;
 
 type Story = StoryObj<typeof Drawer>;
 
-export const Default: Story = {
-  args: {},
+export const Secondary: Story = {
+  args: {
+    open: true,
+    onClose: () => console.log('Drawer closed'),
+    children: (
+      <div className="space-y-4">
+        <h2 className="text-2xl font-semibold text-text-primary">
+          Secondary Drawer
+        </h2>
+        <p className="text-text-primary">
+          This a secondary drawer. You can put any content here.
+        </p>
+        <p className="text-text-primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.
+        </p>
+        <p className="text-text-primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.
+        </p>
+      </div>
+    ),
+  },
+};
+
+export const Primary: Story = {
+  args: {
+    open: true,
+    onClose: () => console.log('Drawer closed'),
+    type: 'primary',
+    children: (
+      <div className="space-y-4">
+        <h2 className="text-2xl font-semibold text-text-primary">
+          Primary Drawer
+        </h2>
+        <p className="text-text-primary">
+          This is a primary drawer with a different background color.
+        </p>
+        <p className="text-text-primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.
+        </p>
+        <p className="text-text-primary">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit.
+        </p>
+      </div>
+    ),
+  },
 };
